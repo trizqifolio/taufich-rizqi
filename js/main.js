4d2a3141a49f064695467d57d0ed14ddccd67807
suwungtandon <suwungtandon@gmail.com>
2020-06-08 19:18:40 +0700

    first

--- js/main.js
+++ js/main.js
@@ -79,7 +79,7 @@ $(document).ready(function() {
 
   $(function() {
     typed.typed({
-      strings: ["Alex Smith.", "Designer.", "Developer.", "Freelancer.", "Photographer"],
+      strings: ["Taufich Rizqi F.", "Graphic Designer.", "Web Developer.", "Freelancer."],
       typeSpeed: 100,
       loop: true,
     });
